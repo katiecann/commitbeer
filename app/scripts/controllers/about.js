91de8e6c1f53dcb77bbdb00b90c980eae9989104
Katie Cann <katietaylorcann@gmail.com>
2015-10-22 19:01:07 -0700

    Re-scaffolded basic app
    Used Yeoman Angular Generator to scaffold app.  Added Buildcontrol.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name commitbeerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the commitbeerApp
+ */
+angular.module('commitbeerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name commitbeerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the commitbeerApp
+ */
+angular.module('commitbeerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
