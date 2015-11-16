bba6fce85e7a9cd12d2299863a79cdd83f1d458e
Katie Cann <katietaylorcann@gmail.com>
2015-11-15 17:43:30 -0800

    Adding Maps ability and additional styling
    Added Angular-Google-Maps to project. Added custom Favicons.  Did some
    styling to Resources View.

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -8,6 +8,23 @@
  * Controller of the commitbeerApp
  */
 angular.module('commitbeerApp')
-  .controller('ResultsCtrl', function () {
+  .controller('ResultsCtrl', function ($scope, uiGmapGoogleMapApi) {
+  	$scope.markers = [];
+  	
+  	uiGmapGoogleMapApi.then(function() {
 
-  });
+    });
+});
+
+/**
+TODO:  Where does this go? ->
+
+.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
+
+*/
