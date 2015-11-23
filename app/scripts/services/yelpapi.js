d19888082a7b50cde1c0deb76a6bbfeecf51ccb2
Katie Cann <katietaylorcann@gmail.com>
2015-11-22 22:38:42 -0800

    Continued hitting head against Yelp API

--- app/scripts/services/yelpapi.js
+++ app/scripts/services/yelpapi.js
@@ -8,6 +8,8 @@
  * Factory in the commitbeerApp.
  */
 
+ //TODO:  FIGURE OUT YELP API SIGNATURE REQUIREMENTS
+/**
 angular.module('commitbeerApp')
   .factory('yelpapi', function ($resource) {
     // Service logic
@@ -15,13 +17,24 @@ angular.module('commitbeerApp')
 
     // Public API here
     //Using Open Weather Map API as placeholder to make sure calls work before figuring our YELP API
-    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:query&mode=json&APPID=1c6578fa25ca04d52842e79521226c8c', {}, {
+    return $resource('https://api.yelp.com/v2/search/?term=wifi&location=:location&limit=20&radius_filter=5000&category_filter=bars,breweries,beer_and_wine', {}, {
       find: {
         method:'GET',
+        url: 'https://api.yelp.com/v2/search/?',
         params:{
-            query: ""
+            location: null,
+            oauth_consumer_key : 'nTnuQt3-And40RQRqw9bow',
+            oauth_token : 'rDntJReWCt89Bpn6Z5vZCZgQ4MMJjcVF',
+            oauth_nonce : '293647061',
+            oauth_timestamp : '1448258587',
+            oauth_signature_method : 'HMAC-SHA1',
+            oauth_version : '1.0'
         },
-        isArray:false
+        consumerSecret: '5fwBbTCHfjZJLrUGPKN95_qRBIE',
+        tokenSecret: 'nmutDCrW-wo60B1bx7pYq0S8bHg',
+        encodedSignature: oauthSignature.generate(method, url, params, consumerSecret, tokenSecret),
+        isArray: false
       }
     });
   });
+*/
